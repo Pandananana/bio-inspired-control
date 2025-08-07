7a96cdfd0c4bf61fe5c9dcd493e7e96965e0a140
Andreas Wendelboe <andreaswendelboe.dk@gmail.com>
2025-08-07 14:33:17 +0200

    Added data randomization and test loss evaluation.

--- exercises_week1/1.3/TODO_train_pytorch.py
+++ exercises_week1/1.3/TODO_train_pytorch.py
@@ -4,47 +4,50 @@ import matplotlib.pyplot as plt
 import torch
 import torch_model
 
-from tqdm import tqdm # progress bar
+from tqdm import tqdm  # progress bar
 
 # Load data
-#data = pickle.load( open( "training_data.p", "rb" ) )
+# data = pickle.load( open( "training_data.p", "rb" ) )
 data = np.loadtxt("robot_data.csv", delimiter=",", skiprows=1)
 
+# Randomize the data
+np.random.shuffle(data)
+
 print(data.shape)
-angles = data[:,:2].T
-end_pos = data[:,2:].T
+angles = data[:, :2].T
+end_pos = data[:, 2:].T
 
 # Use GPU?
-device = 'cpu'
+device = "cpu"
 if torch.cuda.is_available():
     print("Using GPU")
-    device = 'cuda'
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    device = "cuda"
+    torch.set_default_tensor_type("torch.cuda.FloatTensor")
 
 x = torch.from_numpy(end_pos.T).float()
 y = torch.from_numpy(angles.T).float()
 
 # TODO split the training set and test set
-x_train = x[:int(0.8 * len(x))]
-y_train = y[:int(0.8 * len(y))]
-x_test = x[int(0.8 * len(x)):]
-y_test = y[int(0.8 * len(y)):]
+x_train = x[: int(0.8 * len(x))]
+y_train = y[: int(0.8 * len(y))]
+x_test = x[int(0.8 * len(x)) :]
+y_test = y[int(0.8 * len(y)) :]
 
 # Eventually normalize the data
 
-if device == 'cuda':
+if device == "cuda":
     x = x.cuda()
     y = y.cuda()
 
 # Define neural network - an example
 model = torch_model.MLPNet(2, 16, 2)
 # model = torch_model.Net(n_feature=2, n_hidden1=h, n_hidden2=h, n_output=2)
-#print(model)
+# print(model)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 loss_func = torch.nn.MSELoss()
-num_epochs = 500000
+num_epochs = 300000
 
-#h = 16
+# h = 16
 g = 0.999
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=g)
 
@@ -52,26 +55,32 @@ l_vec = np.zeros(num_epochs)
 
 for t in tqdm(range(num_epochs)):
     # TODO Train network
-    prediction = model(x_train) # Forward pass prediction. Saves intermediary values required for backwards pass
-    loss = loss_func(prediction, y_train) # Computes the loss for each example, using the loss function defined above
-    optimizer.zero_grad() # Clears gradients from previous iteration
-    loss.backward() # Backpropagation of errors through the network
-    optimizer.step() # Updating weights
+    prediction = model(
+        x_train
+    )  # Forward pass prediction. Saves intermediary values required for backwards pass
+    loss = loss_func(
+        prediction, y_train
+    )  # Computes the loss for each example, using the loss function defined above
+    optimizer.zero_grad()  # Clears gradients from previous iteration
+    loss.backward()  # Backpropagation of errors through the network
+    optimizer.step()  # Updating weights
     scheduler.step()
 
     l = loss.data
-    if device == 'cuda':
+    if device == "cuda":
         l = l.cpu()
-    #print(l.numpy())
+    # print(l.numpy())
     l_vec[t] = l.numpy()
 
-# TODO Test the network
-
+    # TODO Test the network
+    prediction = model(x_test)
+    loss = loss_func(prediction, y_test)
+    print(f"Test loss: {loss.item()}")
 
 plt.plot(l_vec)
-plt.yscale('log')
+plt.yscale("log")
 
-torch.save(model.state_dict(), 'trained_model.pth')
+torch.save(model.state_dict(), "trained_model.pth")
 plt.show()
 
 
