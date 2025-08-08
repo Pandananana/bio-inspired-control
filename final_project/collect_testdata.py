c2874015ea6a8883f20c63bba3736ec21584d87e
Leona-Vo <s250471@dtu.dk>
2025-08-08 13:40:57 +0200

    Testdata 1 for final project

--- final_project/collect_testdata.py
+++ final_project/collect_testdata.py
@@ -14,6 +14,18 @@ print(cam.read())
 
 i = 0
 
+def rotate_and_resize(frame):
+    """Rotate the frame and resize it to fit the window."""
+    # Rotate the frame
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+    # Resize the frame to fit the window
+    return cv2.resize(frame, (360, 640))
+
+def save_data(data, filename="final_project_data.csv"):
+    """Save collected data to a CSV file."""
+    np.savetxt(filename, data, delimiter=",", header="theta1,theta2,x,y,r", comments='')
+
 def locate(img):
     frame_to_thresh = cv2.cvtColor(img, cv2.COLOR_BGR2LAB) # LAB
     thresh = cv2.inRange(frame_to_thresh, (152, 130, 91), (255, 170, 121))
@@ -35,15 +47,16 @@ def locate(img):
     else:
         x = None
         y = None
+        radius = None
 
-    return x, y
+    return x, y, radius
 
 # Initialization of the camera. Wait for sensible stuff
 def initialize_camera(cam):
     while True:
         frame = ct.capture_image(cam)
 
-        x, _ = locate(frame)
+        x, _, _ = locate(frame)
 
         if x is not None:
             break     
@@ -87,21 +100,33 @@ class TestClass:
     def __init__(self, num_datapoints):
         self.i = 0
         self.num_datapoints = num_datapoints
-        self.data = np.zeros( (num_datapoints, 4) )
+        self.data = np.zeros( (num_datapoints, 5) )
         self.time_of_move = datetime.datetime.now()
 
-    def go(self):
+    def go(self, skip=False):
+        if skip:
+            self.i += 1
+            if self.i >= num_datapoints:
+                return True
+            api.setPos(thetas[0,self.i], thetas[1,self.i], module)
+            self.time_of_move = datetime.datetime.now()
+            return False
+
         if self.i >= num_datapoints:
             return True
         
         img = ct.capture_image(cam)
-        x, y = locate(img)
-        if (datetime.datetime.now() - self.time_of_move).total_seconds() > 2.0:
+
+        img = rotate_and_resize(img)
+
+        x, y, size = locate(img)
+        if (datetime.datetime.now() - self.time_of_move).total_seconds() > 1.0:
             if x is not None:
                 print(x, y)
                 tmeas1 = api.getPos(0,module)
                 tmeas2 = api.getPos(1,module)
-                self.data[self.i,:] = np.array([tmeas1, tmeas2, x, y])
+                self.data[self.i,:] = np.array([tmeas1, tmeas2, x, y, size])
+                save_data(self.data)
                 self.i += 1
 
                 # set new pos
@@ -110,26 +135,31 @@ class TestClass:
                     self.time_of_move = datetime.datetime.now()
             else:
                 print("Obj not found")
-        
-        # Display the frame
-        cv2.imshow("Droidcam Feed", img)
 
         return False
 
 test = TestClass(num_datapoints)
 
 while True:
-    k = cv2.waitKey(0) & 0xFF  # Wait indefinitely for key press
+    img = ct.capture_image(cam)
+
+    img = rotate_and_resize(img)
+
+    x, y, _ = locate(img)
+    cv2.imshow("Droidcam Feed", img)
     
-    if k == ord(' '):  # Space key pressed
-        if test.go():  # Returns True when all datapoints collected
+    k = cv2.waitKey(1) & 0xFF  # Wait for key press
+
+    if k == ord(' '): 
+        if test.go() == True:
             break
+    elif k == ord('s'): #skip this position
+        test.go(skip=True)
     elif k == 27:  # ESC key pressed
         print("Exiting early...")
         break
     
-# Save collected data to CSV
-np.savetxt("final_project_data.csv", test.data, delimiter=",", header="theta1,theta2,x,y", comments='')
+
 
 print('Terminating')
 api.terminate()
