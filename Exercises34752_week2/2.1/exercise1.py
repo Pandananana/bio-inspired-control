7b96ec88c58263441b896a6efce13aed15a3d0ae
Leona-Vo <s250471@dtu.dk>
2025-08-11 11:25:01 +0200

    2.1.1

--- Exercises34752_week2/2.1/exercise1.py
+++ Exercises34752_week2/2.1/exercise1.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 ## Initialization
 # Length of simulation (time steps)
 simlen = 30
+
+# TODO define the time delay
+delay = [0,1,2,3]
+
 # Output
-y = np.zeros((simlen))
+y = np.zeros((simlen, len(delay)))
 # Target
 target = 0.0
 
@@ -13,20 +17,26 @@ target = 0.0
 K = 1
 
 # Set first output
-y[0] = 1
+y[0, :] = 1
 
-# TODO define the time delay
+
 
 ## Simulation
 for t in range(simlen-1):
-    # Compute output
-    # TODO include the time delay
-    u = K * (target - y[t])
-    y[t+1]=0.5*y[t] + 0.4*u # 1st order dynamics
+    for d in delay:
+        # Compute output
+        # TODO include the time delay
+
+        if t - d < 0:
+            u = K * (target - y[0, d])
+        else:
+            u = K * (target - y[t - d, d])
+        y[t+1, d]=0.5*y[t,d] + 0.4*u # 1st order dynamics
 
 ## Plot
 time = range(simlen)
-plt.plot(time, y)
+for d in delay:
+    plt.plot(time, y[:, d], label=f'delay {d}')
 plt.xlabel('time step')
 plt.ylabel('y')
 plt.show()
