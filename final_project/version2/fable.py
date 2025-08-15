e7d982592ee10fa54f81e8fc6ea15001cca1b6cd
Andreas Wendelboe <andreaswendelboe.dk@gmail.com>
2025-08-15 14:12:49 +0200

    Moved robot functions to fable class

--- final_project/version2/fable.py
+++ final_project/version2/fable.py
@@ -6,31 +6,67 @@ from spatialmath import SE3
 
 
 class Fable:
-    def __init__(self):
+    def __init__(self, connection=True):
         ## Fable API
         self.api = api
-        self.api.setup(blocking=True)
-        moduleids = self.api.discoverModules()
-        self.module = moduleids[0] if moduleids else None
-        print("Found modules: ", moduleids)
-        print("Battery: ", self.getBattery())
+        if connection:
+            self.api.setup(blocking=True)
+            moduleids = self.api.discoverModules()
+            self.module = moduleids[0] if moduleids else None
+            print("Found modules: ", moduleids)
+            print("Battery: ", self.getBattery())
 
         ## Robotics Toolbox
         self.robot = rtb.DHRobot(
             [
-                rtb.RevoluteDH(d=0, a=5, alpha=np.pi / 2, qlim=[-np.pi / 2, np.pi / 2]),
-                rtb.RevoluteDH(d=0, a=20, alpha=0, qlim=[-np.pi / 2, np.pi / 2]),
+                rtb.RevoluteDH(
+                    d=0,
+                    a=5,
+                    alpha=np.deg2rad(90),
+                    qlim=[np.deg2rad(-90), np.deg2rad(90)],
+                ),
+                rtb.RevoluteDH(
+                    d=0,
+                    a=9.5,
+                    alpha=np.deg2rad(-90),
+                    qlim=[np.deg2rad(-90), np.deg2rad(90)],
+                ),
+                rtb.PrismaticDH(a=0, theta=0, alpha=0, qlim=[0, 100]),
             ],
             name="Fable",
             base=SE3(0, 0, 22) * SE3.RPY(0, -np.deg2rad(90), -np.deg2rad(90)),
         )
 
-    def moveToPosition(self, tau_1, tau_2):
+    def setMotorAngles(self, tau_1, tau_2):
         api.setPos(tau_1, tau_2, self.module)
 
         # Wait until both motors stop moving
         while api.getMoving(0, self.module) or api.getMoving(1, self.module):
             time.sleep(0.01)  # Small delay to avoid busy waiting
+
+    def setLaserPosition(self, position):
+        angles = self.inverseKinematics(position)
+        self.setMotorAngles(np.rad2deg(angles[0]), np.rad2deg(angles[1]))
+
+    def inverseKinematics(self, point):
+        sol = self.robot.ik_LM(
+            Tep=point,
+            mask=[
+                1,
+                1,
+                1,
+                0,
+                0,
+                0,
+            ],  # Only consider position (x,y,z), ignore orientation
+            tol=1e-6,
+            slimit=1000,
+        )
+        if sol[1]:
+            return sol[0]
+        else:
+            print("No solution found")
+            exit()
 
     def getBattery(self):
         return self.api.getBattery(self.module)
--- final_project/version2/main.py
+++ final_project/version2/main.py
@@ -6,47 +6,18 @@ from fable import Fable
 import roboticstoolbox as rtb
 from spatialmath import SE3
 
-fable = Fable()
+fable = Fable(connection=False)
 
-robot = rtb.DHRobot(
-    [
-        rtb.RevoluteDH(
-            d=0, a=5, alpha=np.deg2rad(90), qlim=[np.deg2rad(-90), np.deg2rad(90)]
-        ),
-        rtb.RevoluteDH(
-            d=0, a=9.5, alpha=np.deg2rad(-90), qlim=[np.deg2rad(-90), np.deg2rad(90)]
-        ),
-        rtb.PrismaticDH(a=0, theta=0, alpha=0, qlim=[0, 100]),
-    ],
-    name="Fable",
-    base=SE3(0, 0, 22) * SE3.RPY(0, -np.deg2rad(90), -np.deg2rad(90)),
-)
 
-point = SE3.Trans(0, 36.5, 0)
-print("Point:")
-print(point)
+point = SE3.Trans(0, 36.5, 36.5)
 
-sol = robot.ik_LM(
-    Tep=point,
-    mask=[1, 1, 1, 0, 0, 0],  # Only consider position (x,y,z), ignore orientation
-    tol=1e-5,
-)  # solve IK
-
-success = sol[1]
-if not success:
-    print("No solution found")
-    exit()
-
-q_pickup = sol[0]
-q_pickup = [np.deg2rad(-45), np.deg2rad(45), 50]
-
-fable.moveToPosition(np.rad2deg(q_pickup[0]), np.rad2deg(q_pickup[1]))
+solution = fable.inverseKinematics(point)
 print("Solution:")
-print(sol[0])
-print("FK:")
-print(robot.fkine(q_pickup))  # FK shows that desired end-effector pose was achieved
+print(solution)
 
-robot.plot(q_pickup, backend="pyplot")
+# fable.setLaserPosition(point)
+
+fable.robot.plot(solution, backend="pyplot")
 
 # Wait for q key input
 while True:
