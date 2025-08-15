f479f6d95d6c515e2d62e3a51e0172fce7255859
Andreas Wendelboe <andreaswendelboe.dk@gmail.com>
2025-08-15 13:52:48 +0200

    Updated robot configuration by adding a prismatic joint and adjusted the target point for movement.

--- final_project/version2/main.py
+++ final_project/version2/main.py
@@ -16,12 +16,13 @@ robot = rtb.DHRobot(
         rtb.RevoluteDH(
             d=0, a=9.5, alpha=np.deg2rad(-90), qlim=[np.deg2rad(-90), np.deg2rad(90)]
         ),
+        rtb.PrismaticDH(a=0, theta=0, alpha=0, qlim=[0, 100]),
     ],
     name="Fable",
     base=SE3(0, 0, 22) * SE3.RPY(0, -np.deg2rad(90), -np.deg2rad(90)),
 )
 
-point = SE3.Trans(-8.286, 6.718, 30.29)
+point = SE3.Trans(0, 36.5, 0)
 print("Point:")
 print(point)
 
@@ -37,13 +38,13 @@ if not success:
     exit()
 
 q_pickup = sol[0]
-# q_pickup = [np.deg2rad(-45), np.deg2rad(45)]
+q_pickup = [np.deg2rad(-45), np.deg2rad(45), 50]
 
 fable.moveToPosition(np.rad2deg(q_pickup[0]), np.rad2deg(q_pickup[1]))
-print("Solution: ", sol[0])
-print(
-    "FK: ", robot.fkine(q_pickup)
-)  # FK shows that desired end-effector pose was achieved
+print("Solution:")
+print(sol[0])
+print("FK:")
+print(robot.fkine(q_pickup))  # FK shows that desired end-effector pose was achieved
 
 robot.plot(q_pickup, backend="pyplot")
 
